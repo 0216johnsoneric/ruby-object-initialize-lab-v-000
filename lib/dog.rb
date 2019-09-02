aefd686f379a15da3bbc2e45d27e7c462b9725e4
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-02 22:43:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,30 @@
+# class Dog
+#   def initialize(name, breed = "Mutt")
+#     @name = name
+#     @breed = breed
+#   end
+#
+#   def name
+#     @name
+#   end
+#
+#   def breed
+#     @breed
+#   end
+# end
+#
+# # name = eric
+# #   def name=(new_name)
+# #     @name = new_name
+# #   end
+# #
+# # end
+
 class Dog
+
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
 
-  def name
-    @name
-  end
-
-  def breed
-    @breed
-  end
 end
-
-# name = eric
-#   def name=(new_name)
-#     @name = new_name
-#   end
-#
-# end
